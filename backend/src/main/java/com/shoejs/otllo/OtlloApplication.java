2acdb981721686bc2ff8668c946cae2b4756af78
Joe Sheen <joe123sheen@hotmail.com>
2024-08-29 17:15:13 +0100

    Initial commit

---
+++ backend/src/main/java/com/shoejs/otllo/OtlloApplication.java
@@ -0,0 +1,13 @@
+package com.shoejs.otllo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtlloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtlloApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shoejs/otllo/OtlloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoejs.otllo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtlloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
