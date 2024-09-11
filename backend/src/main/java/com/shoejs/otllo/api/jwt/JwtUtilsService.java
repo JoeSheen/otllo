a4203d183fd44ff43c46421a6975efbf508c3632
Joe Sheen <joe123sheen@hotmail.com>
2024-09-11 21:47:58 +0100

    Initial JWT authentication implementation

--- backend/src/main/java/com/shoejs/otllo/api/jwt/JwtUtilsService.java
+++ backend/src/main/java/com/shoejs/otllo/api/jwt/JwtUtilsService.java
@@ -1,8 +1,17 @@
 package com.shoejs.otllo.api.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.crypto.SecretKey;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Service
 public class JwtUtilsService {
@@ -10,9 +19,31 @@ public class JwtUtilsService {
     @Value("${api.jwt.secret}")
     private String secret;
 
-    private static final String AUTH_HEADER = HttpHeaders.AUTHORIZATION;
-
-    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String BEARER = "Bearer";
 
     private static final String ISSUER = "OTLLO";
+
+    private static final String AUDIENCE = "otllo.com";
+
+    public String extractTokenFromRequest(HttpServletRequest request) {
+        String jwt = null;
+        String authHeader = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith(BEARER)) {
+            jwt = authHeader.substring(7);
+        }
+        return jwt;
+    }
+
+    public Claims verifyToken(String token) {
+        return Jwts.parser().verifyWith(getSecretSigningKey()).requireIssuer(ISSUER)
+                .requireAudience(AUDIENCE).build().parseSignedClaims(token).getPayload();
+    }
+
+    public String generateToken(String username) {
+        return "";
+    }
+
+    private SecretKey getSecretSigningKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secret));
+    }
 }
--- backend/src/main/java/com/shoejs/otllo/api/jwt/filter/JwtAuthenticationRequestFilter.java
+++ backend/src/main/java/com/shoejs/otllo/api/jwt/filter/JwtAuthenticationRequestFilter.java
@@ -1,13 +1,18 @@
 package com.shoejs.otllo.api.jwt.filter;
 
 import com.shoejs.otllo.api.jwt.JwtUtilsService;
+import com.shoejs.otllo.api.user.User;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,6 +31,33 @@ public class JwtAuthenticationRequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        //exceptionResolver.resolveException(...);
+        try {
+            String token = jwtUtilsService.extractTokenFromRequest(request);
+            Claims claims = jwtUtilsService.verifyToken(token);
+            if (!claims.isEmpty()) {
+                String subject = claims.getSubject();
+                if (subject != null && !subject.isBlank()) {
+                    User user = (User) userDetailsService.loadUserByUsername(subject);
+                    if (user != null && isUnauthenticated()) {
+                        successfulAuthentication(user, request);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            // TODO: add exceptionResolver to handle any exceptions thrown during jwt filter
+            // exceptionResolver...
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isUnauthenticated() {
+        return SecurityContextHolder.getContext().getAuthentication() == null;
+    }
+
+    private void successfulAuthentication(User user, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(user, user, user.getAuthorities());
+        token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(token);
     }
 }
