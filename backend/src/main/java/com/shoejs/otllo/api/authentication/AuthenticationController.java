e86a995bd176121c5dba2929aaf481e94d9217a8
Joe Sheen <joe123sheen@hotmail.com>
2024-10-09 20:33:46 +0100

    Started adding class and method comments to code

--- backend/src/main/java/com/shoejs/otllo/api/authentication/AuthenticationController.java
+++ backend/src/main/java/com/shoejs/otllo/api/authentication/AuthenticationController.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller responsible for authenticating a user. This Class exposes post methods for signup and login.
+ */
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -12,12 +15,24 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
+    /**
+     * Method used to authenticate a user on signup
+     *
+     * @param credentialsDto object used for creating a new user
+     * @return resource produced upon successful authentication
+     */
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public AuthenticationDetailsDto signup(@Valid @RequestBody SignupCredentialsDto credentialsDto) {
         return authenticationService.signup(credentialsDto);
     }
 
+    /**
+     * Method used to authenticate a user who is already known to the application and is returning
+     *
+     * @param credentialsDto object used to authenticate the user on login
+     * @return resource produced upon successful authentication
+     */
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     public AuthenticationDetailsDto login(@Valid @RequestBody LoginCredentialsDto credentialsDto) {
